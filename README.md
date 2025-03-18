e59ceb824976cbbabb981321df3b052b050dbe28
Rio014 <rio014.null@gmail.com>
2025-03-18 11:21:12 -0500

    finished main game

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 - We created player scene, mob scene separately, then use it in the main game scene by instantiate it.
 
-- The reason we instantiate player scene this way (as opposed to do the same way as mob) is because there is only one player in this game. 
+- The reason we instantiate player scene this way (as opposed to do the same way as mob) is because there is only one player in this game.
+
+nice design 
+- Have HUD emits signal to main so that we know when to start game. In this tutorial, they use clicking start button as a signal.
+- If we want an action to applies to all entity (same kind) then we can group them under one name and then apply the action.
--- main.gd
+++ main.gd
@@ -5,46 +5,56 @@ var score
 
 
 func game_over():
-    $ScoreTimer.stop()
-    $MobTimer.stop()
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+	$HUD.show_game_over()
 
 func new_game():
-    score = 0
-    $Player.start($StartPosition.position)
-    $StartTimer.start()
+	# remove all mobs when we start new game
+	get_tree().call_group("mobs", "queue_free")
+
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+	$HUD.update_score(score)
+	$HUD.show_message("Get ready")
+
 
 
 func _on_start_timer_timeout():
-    $MobTimer.start()
-    $ScoreTimer.start()
+	$MobTimer.start()
+	$ScoreTimer.start()
 
 func _on_score_timer_timeout():
-    score += 1
+	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
-    # create new mob
-    var mob = mob_scene.instantiate()
+	# create new mob
+	var mob = mob_scene.instantiate()
 
-    # choose random location on Path2D
-    var mob_spawn_location = $MobPath/MobSpawnLocation
-    mob_spawn_location.progress_ratio = randf()
-    #print(mob_spawn_location)
+	# choose random location on Path2D
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	#print(mob_spawn_location)
 
-    # set mob's position to random location
-    mob.position = mob_spawn_location.position
+	# set mob's position to random location
+	mob.position = mob_spawn_location.position
 
-    var direction = mob_spawn_location.rotation + PI/2
-    # add mob rotation to make it more random
-    direction += randf_range(-PI/4, PI/4)
-    mob.rotation = direction
+	var direction = mob_spawn_location.rotation + PI/2
+	# add mob rotation to make it more random
+	direction += randf_range(-PI/4, PI/4)
+	mob.rotation = direction
 
-    var velocity = Vector2(randf_range(150,250), 0)
-    mob.linear_velocity = velocity.rotated(direction)
+	var velocity = Vector2(randf_range(150,250), 0)
+	mob.linear_velocity = velocity.rotated(direction)
 
-    # spawn mob by adding it to main scene
-    add_child(mob)
+	# spawn mob by adding it to main scene
+	add_child(mob)
 
 
 func _ready():
-    #new_game()
-    pass
+	#new_game()
+	pass
